17497e446e4dd10e0ffc08a2a4986d768cfdb7b3
Tom Grossi <TheAutomaTom@gmail.com>
2023-05-09 18:44:21 -0500

    Add AGGridv View

--- packages/renderer/src/views/AGGrid.vue
+++ packages/renderer/src/views/AGGrid.vue
@@ -12,11 +12,11 @@ import { AgGridVue } from "ag-grid-vue3";
 const _app = useAppState();
 
 const columnDefs= ref([
-    { headerName: "Desc", field: "name" },
-    { headerName: "Ref", field: "reference" },
-    { headerName: "Qty", field: "quantity" },
-    { headerName: "UOM", field: "uom" },
-    { headerName: "Costs", field: "costs" }
+    { headerName: "Desc", field: "name", editable: true },
+    { headerName: "Ref", field: "reference", editable: true },
+    { headerName: "Qty", field: "quantity", editable: true },
+    { headerName: "UOM", field: "uom", editable: true },
+    { headerName: "Costs", field: "costs", editable: true }
   ]);
 const rowData= ref(_app.Project.takeoff.lineItems);
 
@@ -29,55 +29,17 @@ const rowData= ref(_app.Project.takeoff.lineItems);
 <template>
   <h1>{{ _app.Project.projectName }}</h1>
   <ag-grid-vue
-    style="width: 500px; height: 200px"
+    style="width: auto;"
     class="ag-theme-alpine"
+    :dom-layout="`autoHeight`"
     :column-defs="columnDefs"
     :row-data="rowData"
   >
   </ag-grid-vue>
 </template>
 <style>
-table {
-  /* width: auto; */
-  width: 100%;
-  height: auto;
-  border: 2px solid grey;
-}
-tr:nth-child(even),
-tr:nth-child(even) input{
-  background-color: #f2f2f2;
-}
-th, td{
-  padding: 4px 8px;
-}
-th {
-  font-size: small;
-  text-align: left;
-  font-weight: bold;
-  background-color: gray;
-  color: white;
-}
-td input {
-  width: 100%;
-  border: none;
-  box-sizing: border-box;
-  font-size: 14px;
-}
-.t-col-right {
-  text-align: end;
-  padding-right: 0;
-  /* max-width: 4em; */
-}
-.t-col-left {
-  text-align: start;
-  padding-left: 0;
-  /* max-width: 3em; */
-}
-.col-narrow {
-  max-width: 2em;
-}
-input{
-  padding:0;
-  margin:0;
-}
+  /* .my-ag-grid-class{
+    width: 100%;
+    height: 100%;
+  } */
 </style>
