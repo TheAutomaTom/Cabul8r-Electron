fd6120b14df8b8a278c712ee61f01ee7e87fb8a1
Tom Grossi <TheAutomaTom@gmail.com>
2023-06-14 17:19:54 -0500

    Tweak models

--- packages/renderer/src/states/PriceBook.state.ts
+++ packages/renderer/src/states/PriceBook.state.ts
@@ -1,5 +1,7 @@
 // import { ref } from "vue";
 import { defineStore } from "pinia";
+import type { Cost } from "../../../ipc-models/Takeoff/LineItemCost/Cost";
+import { ref } from "vue";
 
 export const usePriceBookState = defineStore("PriceBookState", () => {
 
@@ -7,15 +9,12 @@ export const usePriceBookState = defineStore("PriceBookState", () => {
   //...
 
   //= Properties ==>
-  //...
+  const PriceBook = ref([] as Cost[]);
 
   //= Public Methods ==>
   //...
 
-
-
-
   return {
-
+    PriceBook
   };
 });
