150c9ba62d655a76086faff855ed96cb4524f875
Tom Grossi <TheAutomaTom@gmail.com>
2023-06-15 11:27:37 -0500

    Seed table with test rows

--- packages/main/src/SqlLite3/connection.ts
+++ packages/main/src/SqlLite3/connection.ts
@@ -1,3 +1,6 @@
+import { CostKind } from "./../../../ipc-models/Takeoff/LineItemCost/CostKind";
+import { Cost } from "../../../ipc-models/Takeoff/LineItemCost/Cost";
+
 import Database from "better-sqlite3";
 
 const dbName = "2306112133";
@@ -17,5 +20,21 @@ export const TryCreateDefaultTable = () => {
   );
   const response = createTableQuery.run();
 
+    seedCostTable();
   return response;
+};
+
+const seedCostTable=()=>{
+  InsertCostRow(new Cost(-1, CostKind.MAT.toString(), 5.99, "Pull, wire, 3-inch"));
+  InsertCostRow(new Cost(-1, CostKind.LAB.toString(), 15.50, "Pull installation"));
+  InsertCostRow(new Cost(-1, CostKind.MAT.toString(), 650, "Cabinets per LF"));
+  InsertCostRow(new Cost(-1, CostKind.LAB.toString(), 800, "Field per Day"));
+};
+
+export const InsertCostRow = (cost: Cost) => {
+  const query = db.prepare(`
+    INSERT OR IGNORE INTO ${testTableName}(Kind, Amount, Name)
+    VALUES ('${cost.kind}', '${cost.amount *100}', '${cost.name}');
+  `);
+  return query.run();
 };
